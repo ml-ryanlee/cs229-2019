e8ea2576b306a6fc08ab0196bb5f1ff0ebebb104
Ryan Lee <38227970+ryanleeme17@users.noreply.github.com>
2023-08-14 20:33:41 -0700

    pset 1 completed

--- ps1/src/p05_featuremap.py
+++ ps1/src/p05_featuremap.py
@@ -1,11 +1,75 @@
 import util
+import sys
 import numpy as np
 import matplotlib.pyplot as plt
 
+from numpy.linalg import inv
+
 np.seterr(all='raise')
-
-
 factor = 2.0
+
+def main(train_path, eval_path):
+    '''
+    Run all experiments
+    '''
+    x_train, y_train = util.load_dataset(train_path, add_intercept=True)
+    x_eval, y_eval = util.load_dataset(eval_path, add_intercept=True)
+    
+    # *** START CODE HERE ***
+    # part b/c) plot polynomial linear regression specified by ks onto training
+    x_max = max(x_train[:,1])
+    x_min = min(x_train[:,1])
+    x_span = np.linspace(x_min,x_max,1000).reshape((1000,1))
+    x_span = util.add_intercept(x_span)
+
+    ks=[3,5,10,20]
+    k_label = ['train data','k=3','k=5','k=10','k=20']
+    ks_line = ['b-','g-','c-','m-']
+    clf = LinearModel()
+    plt.figure()
+    plt.plot(x_train[:,1:],y_train,'o')
+    for i in range(len(ks)):
+        x_train_map = create_poly(ks[i],x_train)
+        clf.fit(x_train_map,y_train)
+        
+        phi_x_span = create_poly(ks[i],x_span)
+        y_span = clf.predict(phi_x_span)
+        
+        plt.plot(x_span[:,1:],y_span, ks_line[i])
+    plt.legend(k_label)
+    plt.xlabel('x attribute')
+    plt.ylabel('y response')
+    plt.title('Training Dataset vs. Predicted Response, polynomial features')
+    plt.show()
+
+    # part d)
+    plt.figure()
+    plt.plot(x_train[:,1:],y_train,'o')
+    
+    # test
+    x_train_map = create_poly(3,x_train)
+    print('x_train after polyfit: ',x_train_map.shape)
+    print('x_train: ','\n',x_train_map)
+    x_train_map = sinetransform(x_train_map) 
+    print('x_train after sine transform: ',x_train_map.shape)
+    print('x_train: ','\n',x_train_map)
+    
+    for i in range(len(ks)):
+        x_train_map = create_poly(ks[i],x_train)
+        x_train_map = sinetransform(x_train_map)
+        clf.fit(x_train_map,y_train)
+        
+        phi_x_span = create_poly(ks[i],x_span)
+        phi_x_span = sinetransform(phi_x_span)
+        y_span = clf.predict(phi_x_span)
+        
+        plt.plot(x_span[:,1:],y_span, ks_line[i])
+    plt.legend(k_label)
+    plt.xlabel('x attribute')
+    plt.ylabel('y response')
+    plt.title('Training Dataset vs. Predicted Response, polynomial features, sin(x)')
+    plt.show()
+    # *** END CODE HERE ***
 
 class LinearModel(object):
     """Base class for linear models."""
@@ -17,87 +81,68 @@ class LinearModel(object):
         """
         self.theta = theta
 
-    def fit(self, X, y):
+    def fit(self, x, y):
         """Run solver to fit linear model. You have to update the value of
         self.theta using the normal equations.
 
         Args:
-            X: Training example inputs. Shape (n_examples, dim).
+            x: Training example inputs. Shape (n_examples, dim).
             y: Training example labels. Shape (n_examples,).
         """
         # *** START CODE HERE ***
+        # 0. initialize theta
+        n = x.shape[1]
+        self.theta = np.zeros(n)
+        
+        # 1. update to theta with normal equations
+        self.theta = inv(x.T@x)@(x.T@y)
+        assert(self.theta.shape == (n,))
         # *** END CODE HERE ***
 
-    def create_poly(self, k, X):
+    def predict(self, x):
+        """
+        Make a prediction given new inputs x.
+        Returns the numpy array of the predictions.
+
+        Args:
+            x: Inputs of shape (n_examples, dim).
+
+        Returns:
+            Outputs of shape (n_examples,).
+        """
+        # *** START CODE HERE ***
+        return x@self.theta
+        # *** END CODE HERE ***
+
+def create_poly(k, x):
         """
         Generates a polynomial feature map using the data x.
         The polynomial map should have powers from 0 to k
         Output should be a numpy array whose shape is (n_examples, k+1)
 
         Args:
-            X: Training example inputs. Shape (n_examples, 2).
+            x: Training example inputs. Shape (n_examples, 2).
         """
         # *** START CODE HERE ***
+        m = len(x)
+        min_k = 2
+        if (k<min_k): return np.copy(x)
+        phi_x = np.copy(x)
+        
+        for i in range(k-1):
+            attr = x[:,-1].reshape((m,1))    
+            phi_x = np.append(phi_x,attr**(i+min_k),axis=1)
+        return phi_x
         # *** END CODE HERE ***
 
-    def create_sin(self, k, X):
-        """
-        Generates a sin with polynomial featuremap to the data x.
-        Output should be a numpy array whose shape is (n_examples, k+2)
-
-        Args:
-            X: Training example inputs. Shape (n_examples, 2).
-        """
-        # *** START CODE HERE ***
-        # *** END CODE HERE ***
-
-    def predict(self, X):
-        """
-        Make a prediction given new inputs x.
-        Returns the numpy array of the predictions.
-
-        Args:
-            X: Inputs of shape (n_examples, dim).
-
-        Returns:
-            Outputs of shape (n_examples,).
-        """
-        # *** START CODE HERE ***
-        # *** END CODE HERE ***
-
-
-def run_exp(train_path, sine=False, ks=[1, 2, 3, 5, 10, 20], filename='plot.png'):
-    train_x,train_y=util.load_dataset(train_path,add_intercept=True)
-    plot_x = np.ones([1000, 2])
-    plot_x[:, 1] = np.linspace(-factor*np.pi, factor*np.pi, 1000)
-    plt.figure()
-    plt.scatter(train_x[:, 1], train_y)
-
-    for k in ks:
-        '''
-        Our objective is to train models and perform predictions on plot_x data
-        '''
-        # *** START CODE HERE ***
-        # *** END CODE HERE ***
-        '''
-        Here plot_y are the predictions of the linear model on the plot_x data
-        '''
-        plt.ylim(-2, 2)
-        plt.plot(plot_x[:, 1], plot_y, label='k=%d' % k)
-
-    plt.legend()
-    plt.savefig(filename)
-    plt.clf()
-
-
-def main(train_path, small_path, eval_path):
-    '''
-    Run all expetriments
-    '''
-    # *** START CODE HERE ***
-    # *** END CODE HERE ***
+def sinetransform(x):
+    #assumes offset column added in x
+    m = len(x)
+    attr = x[:,1].reshape((m,1))
+    attr_sin = np.sin(attr).reshape((m,1))
+    assert(attr_sin.shape == (m,1)) 
+    return np.append(x,attr_sin,axis=1)
 
 if __name__ == '__main__':
     main(train_path='train.csv',
-        small_path='small.csv',
         eval_path='test.csv')
--- ps1/src/run.py
+++ ps1/src/run.py
@@ -4,8 +4,8 @@ from p01b_logreg import main as p01b
 from p01e_gda import main as p01e
 from p02abf_posonly import main as p02
 from p03d_poisson import main as p03
-from p05b_lwr import main as p05b
-from p05c_tau import main as p05c
+from p05_featuremap import main as p05
+# from p05c_tau import main as p05c
 
 parser = argparse.ArgumentParser()
 parser.add_argument('p_num', nargs='?', type=int, default=0,
@@ -46,12 +46,8 @@ if args.p_num == 0 or args.p_num == 3:
 
 # Problem 5
 if args.p_num == 0 or args.p_num == 5:
-#     p05b(tau=5e-1,
-#          train_path='../data/ds5_train.csv',
-#          eval_path='../data/ds5_valid.csv')
-
-    p05c(tau_values=[3e-2, 5e-2, 1e-1, 5e-1, 1e0, 1e1],
-         train_path='../data/ds5_train.csv',
-         valid_path='../data/ds5_valid.csv',
-         test_path='../data/ds5_test.csv',
-         pred_path='output/p05c_pred.txt')
+    p05(train_path='../data/ds5_train.csv',
+         eval_path='../data/ds5_valid.csv')
+    
+    p05(train_path='../data/ds5_small.csv',
+         eval_path='../data/ds5_valid.csv')
